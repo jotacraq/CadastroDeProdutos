ea5adb692ab2325349c24df5ad24aab7d7a5823f
jotacraq <jpalvesassuncao13@outlook.com>
2026-01-18 16:34:20 -0300

    feat: created a entity in bd from ProdutoModel

---
+++ src/main/java/dev/jotacraq/CadastroDeProdutos/ProdutoModel.java
@@ -0,0 +1,71 @@
+package dev.jotacraq.CadastroDeProdutos;
+
+import jakarta.persistence.*;
+
+
+// Entity ele transforma uma classe em uma entidade do BD.
+@Entity
+@Table(name = "tb_cadastro")
+public class ProdutoModel {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String modelo;
+    private String marca;
+    private String serie;
+    private float preco;
+    private String tipo;
+
+    public ProdutoModel() {
+    }
+
+    public ProdutoModel(String modelo, String marca, String serie, float preco, String tipo) {
+        this.modelo = modelo;
+        this.marca = marca;
+        this.serie = serie;
+        this.preco = preco;
+        this.tipo = tipo;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getSerie() {
+        return serie;
+    }
+
+    public void setSerie(String serie) {
+        this.serie = serie;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+}
